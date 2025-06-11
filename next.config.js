a2dfe8bccff69a2a3b32efc0c6554fcce84ba5c5
Android Studio <devandroid@ort>
2025-06-11 11:00:52 -0300

    a2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    // Enable Node.js runtime support for middleware
-    nodeMiddleware: true,
-  },
+  // otras configuraciones
+  // experimental: {
+  //   nodeMiddleware: true, // <- esta línea fuera
+  // },
 };
 
 module.exports = nextConfig;
