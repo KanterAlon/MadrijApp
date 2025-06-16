00ebbcb690998bab48228f3e79f787396c0a1a02
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2025-06-16 07:15:26 -0300

    Delete sql/migrations directory

--- sql/migrations/20240811_add_activo_to_materiales.sql
+++
@@ -1,5 +0,0 @@
--- Adds activo column to materiales for soft delete
-ALTER TABLE public.materiales
-  ADD COLUMN activo boolean DEFAULT true;
-
-UPDATE public.materiales SET activo = true WHERE activo IS NULL;
